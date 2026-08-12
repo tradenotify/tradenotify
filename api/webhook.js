e0ca596418dc848306e57bbfb26d83f24751a183
Miguel Angel García Caballero <miguelforall@gmail.com>
2026-08-12 19:50:36 +0200

    fix: configurar variable de entorno para api key sin secretos

--- api/webhook.js
+++ api/webhook.js
@@ -53,7 +53,7 @@ module.exports = async (req, res) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Key os_v2_app_7oz6t4dv7be5lpnnffw2uj4k2dcpwvdgqziuge4bpv5cv23hntc2cx2ujipmhn26dfi64gibgsjhvq66ccitm5qoaqaer5o4sft3a5i"
+        "Authorization": "Key " + process.env.ONESIGNAL_API_KEY
       },
       body: JSON.stringify(onesignalPayload)
     });
